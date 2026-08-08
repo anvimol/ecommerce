e37c34ea73f0f8c015b287b1e420082d4e5d85c2
anvimol <anvimol@gmail.com>
2026-08-08 17:12:56 +0200

    Creados los metodos para Actualizar y Eliminar

--- src/main/java/com/anvimol/ecommerce/controller/ProductoController.java
+++ src/main/java/com/anvimol/ecommerce/controller/ProductoController.java
@@ -1,13 +1,19 @@
 package com.anvimol.ecommerce.controller;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 // import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.anvimol.ecommerce.model.Producto;
 import com.anvimol.ecommerce.model.Usuario;
@@ -43,5 +49,53 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto= new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);
+		producto= optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto", producto);
+		
+		return "productos/edit";
+	}
+
+    @PostMapping("/update")
+	public String update(Producto producto) throws IOException {
+		/* Producto p= new Producto();
+		p=productoService.get(producto.getId()).get(); */
+		
+		/* if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagem
+			
+			producto.setImagen(p.getImagen());
+		}else {// cuando se edita tbn la imagen			
+			//eliminar cuando no sea la imagen por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		} */
+		// producto.setUsuario(p.getUsuario());
+		productoService.update(producto);		
+		return "redirect:/productos";
+	}
+
+    @GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		
+		/* Producto p = new Producto();
+		p=productoService.get(id).get();
+		
+		//eliminar cuando no sea la imagen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		} */
+		
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
     
 }
