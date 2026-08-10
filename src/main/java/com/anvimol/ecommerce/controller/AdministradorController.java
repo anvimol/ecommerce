760c9975e0f4cb51633eeeb9551e0844780a0ec4
anvimol <anvimol@gmail.com>
2026-08-10 12:49:41 +0200

    Funcionalidad para ver los ordenes por el Admin

--- src/main/java/com/anvimol/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/anvimol/ecommerce/controller/AdministradorController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.anvimol.ecommerce.model.Producto;
+import com.anvimol.ecommerce.service.IOrdenService;
+import com.anvimol.ecommerce.service.IUsuarioService;
 import com.anvimol.ecommerce.service.ProductoService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,12 @@ public class AdministradorController {
     @Autowired
     private ProductoService productoService;
 
+    @Autowired
+    private IUsuarioService usaurioService;
+
+    @Autowired
+	private IOrdenService ordenService;
+
     @GetMapping("")
     public String home(Model model) {
 
@@ -27,5 +35,17 @@ public class AdministradorController {
         model.addAttribute("productos", productos);
         return "administrador/home";
     }
+
+    @GetMapping("/usuarios")
+    public String usuarios(Model model) {
+        model.addAttribute("usuarios", usaurioService.findAll());
+        return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("ordenes", ordenService.findAll());
+		return "administrador/ordenes";
+	}
     
 }
